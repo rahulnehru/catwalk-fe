85fe10d8ade8985b9b97f1d65a5d6889067543c5
alan.hutcheson <alanhutcheson@deloitte.co.uk>
2019-01-14 21:32:47 +0000

    Added hot reloading

--- src/index.js
+++ src/index.js
@@ -6,7 +6,16 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = Component => ReactDOM.render(<Component />, document.getElementById('root'));
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    render(NextApp);
+  });
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
