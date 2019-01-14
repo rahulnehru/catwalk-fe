3a7d9b89e6118f0af5ed00dafa63a58d72196b95
rahul.nehru <rnehru@deloitte.co.uk>
2019-01-14 21:18:02 +0000

    Removes dummy table

--- src/App.js
+++ src/App.js
@@ -8,35 +8,6 @@ class App extends Component {
             <div className="App">
                 <header className="App-header"><h1>Catwalk</h1></header>
                 <ReleaseTable/>
-                <table className="table">
-                    <thead>
-                    <tr>
-                        <th scope="col">Build 1</th>
-                        <th scope="col">Build 2</th>
-                        <th scope="col">Build 3</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <th scope="row">Customer</th>
-                        <td>Green</td>
-                        <td>Green</td>
-                        <td>Red</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Catalogue</th>
-                        <td>Green</td>
-                        <td>Red</td>
-                        <td>Red</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Payments</th>
-                        <td>Green</td>
-                        <td>Green</td>
-                        <td>Green</td>
-                    </tr>
-                    </tbody>
-                </table>
             </div>
         );
     }
